aeb7dfb4965d98b5d982adbdcb5992b478a4cfc1
Andrew Zarei <andrew.zarei.09@gmail.com>
2024-04-23 11:50:48 +0100

    flask exercise and challenges using GET/PUSH

--- app.py
+++ app.py
@@ -16,6 +16,51 @@ app = Flask(__name__)
 def get_emoji():
     return ":)"
 
+@app.route('/submit', methods=['POST'])
+def post_submit():
+    # print(request.form)
+    # print(request.args)
+    name = request.form['name']
+    message = request.form['message']
+    
+    return f'Thanks {name}, you sent this message: "{message}"'
+
+@app.route('/wave', methods=['GET'])
+def get_wave():
+    name = request.args['name']
+    return f'I am waving at {name}'
+
+@app.route('/count_vowels', methods=['POST'])
+def post_count_vowels():
+    text = request.form['text']
+    vowel_number = 0
+    for char in text:
+        if char in 'aeiou':
+            vowel_number += 1
+    return f'There are {vowel_number} vowels in "{text}"' 
+
+@app.route('/sort_names', methods=['POST'])
+def sort_names():
+    if 'names' not in request.form:
+        return "You didn't submit any names!", 400
+    name_string = request.form['names']
+    name_list = name_string.split(',')
+    sorted_name_list = sorted(name_list)
+    result = ','.join(sorted_name_list)
+    return result
+
+@app.route('/names', methods=['GET'])
+def get_names_and_sort():
+    old_name_list = ['Alice', 'Julia', 'Karim']
+    new_name_string = request.args['add']
+    new_name_list = new_name_string.split(',')
+    name_list = old_name_list + new_name_list
+    sorted_names = sorted(name_list)
+    result = ', '.join(sorted_names)
+    return result
+
+
+
 # This imports some more example routes for you to see how they work
 # You can delete these lines if you don't need them.
 from example_routes import apply_example_routes
--- tests/test_app.py
+++ tests/test_app.py
@@ -10,4 +10,56 @@ def test_get_emoji(web_client):
     assert response.status_code == 200
     assert response.data.decode("utf-8") == ":)"
 
+def test_post_submit(web_client):
+    response = web_client.post('/submit', data={'name': 'Leo', 'message': 'Hello World'})
+    assert response.status_code == 200
+    assert response.data.decode("utf-8") == 'Thanks Leo, you sent this message: "Hello World"'
+
+def test_get_wave(web_client):
+    response = web_client.get('/wave?name=Leo')
+    assert response.status_code == 200
+    assert response.data.decode("utf-8") == 'I am waving at Leo'
+
+"""
+When: I make a POST request to /count_vowels
+And: I send "eee" as the body parameter text
+Then: I should get a 200 response with 3 in the message
+"""
+def test_post_count_vowels_eee(web_client):
+    response = web_client.post('/count_vowels', data={'text': 'eee'})
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'There are 3 vowels in "eee"'
+
+"""
+When: I make a POST request to /count_vowels
+And: I send "eunoia" as the body parameter text
+Then: I should get a 200 response with 5 in the message
+"""
+def test_post_count_vowels_eunoia(web_client):
+    response = web_client.post('/count_vowels', data={'text': 'eunoia'})
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'There are 5 vowels in "eunoia"'
+
+"""
+When: I make a POST request to /count_vowels
+And: I send "mercurial" as the body parameter text
+Then: I should get a 200 response with 4 in the message
+"""
+def test_post_count_vowels_mercurial(web_client):
+    response = web_client.post('/count_vowels', data={'text': 'mercurial'})
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'There are 4 vowels in "mercurial"'   
+
+
+def test_sort_names(web_client):
+    response = web_client.post('/sort_names', data={'names': 'Joe,Alice,Zoe,Julia,Kieran'})
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'Alice,Joe,Julia,Kieran,Zoe'
+
+def test_get_names_and_sort(web_client):
+    response = web_client.get('/names?add=Eddie,Leo')
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'Alice, Eddie, Julia, Karim, Leo'
+    
+
 # === End Example Code ===
